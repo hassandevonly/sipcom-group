f6fc69019ab5f37947daefee0596307978bff9bb
hassandevonly <hassanebangourabusiness2707@gmail.com>
2025-05-31 20:00:58 +0000

    Finalisation de la partie accueil

--- src/app/social/includes/header/header.component.ts
+++ src/app/social/includes/header/header.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  menuOpen = false;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
 }
