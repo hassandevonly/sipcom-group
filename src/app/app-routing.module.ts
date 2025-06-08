b0f830c7613b28c3570f72970c0ea96c546876ba
hassandevonly <hassanebangourabusiness2707@gmail.com>
2025-06-08 14:42:09 +0000

    Implementation de la partie blog

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccueilComponent } from './social/accueil/accueil.component';
+import { ContactComponent } from './social/contact/contact.component';
+import { ServicesComponent } from './social/services/services.component';
+import { BlogComponent } from './social/blog/blog.component';
 
 const routes: Routes = [
   {
@@ -11,7 +14,19 @@ const routes: Routes = [
   {
     path: 'Accueil',
     component: AccueilComponent
-  }
+  },
+  {
+    path: 'Contact',
+    component: ContactComponent
+  },
+  {
+    path: 'Services',
+    component: ServicesComponent
+  },
+  {
+    path: 'Blog',
+    component: BlogComponent
+  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './social/includes/header/header.component';
 import { FooterComponent } from './social/includes/footer/footer.component';
 import { AccueilComponent } from './social/accueil/accueil.component';
+import { ContactComponent } from './social/contact/contact.component';
+import { ServicesComponent } from './social/services/services.component';
+import { BlogComponent } from './social/blog/blog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    AccueilComponent
+    AccueilComponent,
+    ContactComponent,
+    ServicesComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
